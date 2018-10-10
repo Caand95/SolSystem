a57023c0f44394b7a4e31d0c66ddf1a029120760
The Med1c <caand95@gmail.com>
2018-10-10 20:46:04 +0200

    FAAAAAAK

--- Source/View/TEST.php
+++ Source/View/TEST.php
@@ -9,22 +9,22 @@ function GetInfo(){
                 "speed" => "0",
                 "hasRings" => "0",
                 "color" => "orange"
-            ),
+              ),
             "Earth" => array(
                 "name" => "Earth",
                 "x" => "-50",
                 "y" => "-30",
                 "size" => "20",
-                "speed" => "4",
+                "speed" => "40",
                 "hasRings" => "true",
                 "color" => "blue"
             ),
-            "Mars" => array(  
+            "Mars" => array(
                 "name" => "Mars",
                 "x" => "20",
                 "y" => "20",
                 "size" => "10",
-                "speed" => "5",
+                "speed" => "50",
                 "hasRings" => "false",
                 "color" => "red"
             )
@@ -38,52 +38,51 @@ $list->Elements = json_decode(json_encode(GetInfo()), FALSE);
 
 <!-- CSS - info fra php -->
 <style>
-    .element-solsystem{
-        position:absolute;
-        top:50%;
-        bottom:50%; 
-        left:50%;
-        right:50%;
+  .element-solsystem{
+      position:absolute;
+      top:50%;
+      bottom:50%;
+      left:50%;
+      right:50%;
+  }
+  .element{
+      position:absolute;
+      border:solid 1px transparent !important;
+      border-radius: 500px;
     }
-    .element{
-        position:absolute;
-        border:solid 1px transparent !important;
-        border-radius: 500px;
-        
-    }
-    <?php
-    foreach($list->Elements as $Element){
-            
-        echo ".element-".$Element->name."{\n";
-        echo    "Height:".($Element->size).";\n";
-        echo    "Width:".($Element->size).";\n";
-        echo    "background-color:".$Element->color.";\n";
-            
-        if($Element->name != "Sol"){
-            if($Element->y < 0){
-                echo    "Bottom:-".( ($list->Elements->Sol->size/2) + (-1*($Element->y)) + ($Element->size/2) ).";\n";
-            } 
-            else 
-            {
-                echo    "Bottom:".( ($list->Elements->Sol->size/2) + ($Element->y) + ($Element->size/2) ).";\n";
-            }
+<?php
+  foreach($list->Elements as $Element){
+      echo "\n#".$Element->name."{\n";
+      echo    "Height:".($Element->size).";\n";
+      echo    "Width:".($Element->size).";\n";
+      echo    "background-color:".$Element->color.";\n";
+
+      if($Element->name != "Sol"){
+          if($Element->y < 0){
+              echo    "Bottom:-".( ($list->Elements->Sol->size/2) + (-1*($Element->y)) + ($Element->size/2) ).";\n";
+          }
+          else
+          {
+              echo    "Bottom:".( ($list->Elements->Sol->size/2) + ($Element->y) + ($Element->size/2) ).";\n";
+          }
             if($Element->x < 0){
                 echo    "Left:-".( ($list->Elements->Sol->size/2) + (-1*($Element->x)) + ($Element->size/2) ).";\n";
-            } 
-            else 
-            {
-                echo    "Left:".( ($list->Elements->Sol->size/2) + ($Element->x) + ($Element->size/2) ).";\n";
-            }
-        } 
-        else 
-        {
-            echo    "Bottom:".((-1*($Element->size/2)) ).";\n";
-            echo    "Left:".((-1*($Element->size/2)) ).";\n";
-        }
-        echo "}";
-        }
-    ?>
+          }
+          else
+          {
+              echo    "Left:".( ($list->Elements->Sol->size/2) + ($Element->x) + ($Element->size/2) ).";\n";
+          }
+      }
+      else
+      {
+          echo    "Bottom:".((-1*($Element->size/2)) ).";\n";
+          echo    "Left:".((-1*($Element->size/2)) ).";\n";
+      }
+    echo "}";
+  }?>
 </style>
+
+
 
 <!-- HTML -->
 <section class="element-solsystem">
@@ -91,13 +90,48 @@ $list->Elements = json_decode(json_encode(GetInfo()), FALSE);
     <!-- PHP -->
     <?php
     foreach($list->Elements as $Element){
-        echo"<div class=\"element element-".$Element->name."\"></div>";
+        echo"<div id=\"".$Element->name."\" class=\"element\"></div>";
+    }
+    ?>
+
+    <!-- JAVASCRIPT -->
+    <script>
+    function FloatInCircle(name, x, y, speed, size) {
+      if(name != "Sol"){
+
+      setInterval(function(){
+
+        var element = document.getElementById(''+name+'');
+        var angle = 1;
+        var w = size;
+        var h = size;
+
+        //x = Math.cos(angle * Math.PI / 180);
+        x = w * Math.cos((Date.now()%60000)/60000 * Math.PI * speed) * (size/2);
+        y = (h * Math.sin((Date.now()%60000)/60000 * Math.PI * speed) * (size/2) )/1.1;
+        //y = Math.sin(angle * Math.PI / 180);
+
+        angle++;
+        document.getElementById(name).style.left=x;
+        document.getElementById(name).style.bottom=y;
+
+        if (angle > 360) {
+            angle = 1;
+        }
+      }, 10);
+      }
+
+    }
+
+
+    </script>
+
+    <!-- PHP - running the animation -->
+    <?php
+    foreach($list->Elements as $Element){
+      echo "<script>
+        FloatInCircle(\"".$Element->name."\",\"".$Element->x."\",\"".$Element->y."\",\"".$Element->speed."\",\"".$Element->size."\");
+      </script>";
     }
     ?>
 </section>
-
-
-<!-- JAVASCRIPT -->
-
-
-
