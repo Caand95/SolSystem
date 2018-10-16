b3fc8a3e4dc71d50b7070a2cb3b9615471cc4ab0
Casper Andersen <casp7654@zbc.dk>
2018-10-16 21:47:23 +0200

    gider ikke den leg længere lige nu så det bliver sådan update

--- Source/View/SolSystemet.php
+++ Source/View/SolSystemet.php
@@ -1,3 +1,132 @@
 <?php //REMOVE WHEN VIEW IS DONE
-    include "./View/testmappe/view1.php";
+    //include "./View/testmappe/view1.php";
 ?>
+<?php require_once "./Scripts/LoadShowPlanets.php"; ?>
+<?php require_once "./Scripts/LoadPlanets.php"; ?>
+
+<!-- CSS - info fra php -->
+<style>
+    .element-solsystem {
+        position: absolute;
+        top: 50%;
+        bottom: 50%;
+        left: 50%;
+        right: 50%;
+    }
+
+    .element {
+        position: absolute;
+        border: solid 1px transparent !important;
+        border-radius: 500px;
+    }
+
+    <?php foreach($list->Elements as $Element) {
+        echo "\n#".$Element->name."{\n";
+        echo "Height:".($Element->size).";\n";
+        echo "Width:".($Element->size).";\n";
+        echo "background-color:".$Element->color.";\n";
+
+        if($Element->name=="Sol") {
+            echo "Bottom:".((-1*($Element->size/2))).";\n";
+            echo "Left:".((-1*($Element->size/2))).";\n";
+        }
+
+        echo "}";
+    }?>
+
+</style>
+
+ <!-- Snask -->
+    <script>
+        function PlanetOrbit(name, speed, distance, size) {
+
+            var PlanetReference = document.getElementById(name);
+            var Reference = document.getElementById("Sol");
+
+            var Planet = {
+                r: distance + size / 2,
+                a: (Math.random() * 5),
+                x: 0,
+                y: 0,
+                s: (speed * 0.001)
+            }
+
+            setInterval(function() {
+
+                Planet.x = Math.cos(Planet.a) * Planet.r;
+                Planet.y = Math.sin(Planet.a) * Planet.r;
+
+                Planet.a += Planet.s;
+                //PlanetPoint.x += PlanetPoint.d;
+                //PlanetPoint.y += PlanetPoint.d;
+
+                PlanetReference.style.left = Planet.x;
+                PlanetReference.style.bottom = Planet.y;
+
+                Planet.a += Planet.s;
+
+                //console.log('Name=%s, Angle=%d, x=%s, y=%s', name, Planet.a * 1000, "" + Planet.x + "", "" + Planet.y + "");
+
+            }, 16);
+        }
+
+    </script>
+<section>
+    <!-- View SideBar -->
+    <section class="sidenav-nav d-flex flex-column align-items-center">
+        <?php
+        // sort på nested objects value (Planet->sunDistance)
+        usort($ListofPlanets,function($first,$second){return $first->sunDistance > $second->sunDistance;});
+
+        // Print nav-item HTML elementer & kald PlanetHover for hvert element
+        foreach($ListofPlanets as $Planet){
+            echo"<div id=\"sidenav-item-$Planet->name\" class=\"sidenav-item d-flex justify-content-center align-items-center\" style=\"border: solid 2px $Planet->hexColor;background:".$Planet->hexColor."75;color:$Planet->hexColor !important\">$Planet->name</div>\n";
+            echo"<script>HoverGlow(\"sidenav-item-$Planet->name\", \"$Planet->name\", \"golden\");</script>\n";
+        }
+
+        // Kald PlanetHover for hvert nav-item element
+        // foreach($ListofPlanets as $Planet){
+        //    echo"<script>PlanetHover(\"sidenav-item-$Planet->name\", \"$Planet->name\");</script>\n";
+        //}
+        ?>
+    </section>
+<!-- View Content -->
+    <section class="element-solsystem" style="">
+        <div id="Sol" class=element style="Left:-30; Bottom:-30; Height:60; Width:60; background-color:Orange;"></div>
+ <?php
+    
+        foreach($ListofShowPlanets->Elements as $Planet){
+            /*PlanetConfig*/
+            foreach($ListofPlanets as $RealPlanets){
+             if($RealPlanets->name == $Planet->name){
+                 $Planet->color = $RealPlanets->hexColor;
+             }  
+            }
+            /*Div as planet element*/
+            echo"<div ".
+                "id=\"$Planet->name\" ".
+                "class=\"element\"".
+                "style=\"".
+                    "Left:0;".
+                    "Bottom:0;".
+                    "background-color:$Planet->color;";
+            if(($Planet->radius*2) > 50){
+            echo    "height:".($Planet->radius).";".
+                    "width:".($Planet->radius).";";
+            } else {
+            echo    "height:".($Planet->radius*4).";".
+                    "width:".($Planet->radius*4).";";
+            }
+            
+            echo    "\"></div>\n";
+            
+            if($Planet->name != "Sol"){
+                /*Script for running the javascript*/
+                echo"<script>\n";
+                echo"PlanetOrbit(\"$Planet->name\", $Planet->speed, $Planet->distance,$Planet->radius)\n";
+                echo"</script>\n";
+            }
+        }
+    ?>
+    </section>
+</section>
--- Source/View/testmappe/View1TestPhysics.php
+++ Source/View/testmappe/View1TestPhysics.php
@@ -1,67 +1,74 @@
+<?php require_once "./Scripts/LoadShowPlanets.php"; ?>
 <?php require_once "./Scripts/LoadPlanets.php"; ?>
 <section class="element-solsystem">
-<!-- JAVASCRIPT -->
-<script>
-    function PlanetOrbit(name, speed, distance, size) {
-        
-        var Planet = document.getElementById(name);
-        var Reference = document.getElementById("Sol");
-        
-        var OrbitPoint = {
-            X:0,
-            Y:0,
-            radius:distance,
-            angle: 0
-        }
-        var floatingPoint = {
-            x: 0,
-            y: 0,
-            speed: ((speed*0.0001)/24)
-        }
+    <!-- JAVASCRIPT -->
+    <script>
+        function PlanetOrbit(name, speed, distance, size) {
 
-        setInterval(function() {
-            floatingPoint.x = OrbitPoint.X + Math.cos(OrbitPoint.angle) * OrbitPoint.radius;
-            floatingPoint.y = OrbitPoint.Y + Math.sin(OrbitPoint.angle) * OrbitPoint.radius;
-            Planet.style.left = floatingPoint.x;
-            Planet.style.bottom = floatingPoint.y;
-            OrbitPoint.angle += floatingPoint.speed;
-            
-            if (OrbitPoint.angle >= 360) {
-                OrbitPoint.angle = 0;
-            }
-            if(name == "Merkur"){
-                console.log('Name=%s, Angle=%d, x=%s, y=%s', name, OrbitPoint.angle, ""+floatingPoint.x+"", ""+floatingPoint.y+"");
+            var PlanetReference = document.getElementById(name);
+            var Reference = document.getElementById("Sol");
+
+            var Planet = {
+                r: distance + size / 2,
+                a: (Math.random() * 5),
+                x: 0,
+                y: 0,
+                s: (speed * 0.001)
             }
 
-        }, 10);
-    }
+            setInterval(function() {
 
-</script>
-   <div id="Sol" class=element style="Left:-30; Bottom:-30; Height:60; Width:60; background-color:Orange;"></div>
-         
+                Planet.x = Math.cos(Planet.a) * Planet.r;
+                Planet.y = Math.sin(Planet.a) * Planet.r;
+
+                Planet.a += Planet.s;
+                //PlanetPoint.x += PlanetPoint.d;
+                //PlanetPoint.y += PlanetPoint.d;
+
+                PlanetReference.style.left = Planet.x;
+                PlanetReference.style.bottom = Planet.y;
+
+                Planet.a += Planet.s;
+
+                console.log('Name=%s, Angle=%d, x=%s, y=%s', name, Planet.a * 1000, "" + Planet.x + "", "" + Planet.y + "");
+
+            }, 10);
+        }
+
+    </script>
+    <div id="Sol" class=element style="Left:-30; Bottom:-30; Height:60; Width:60; background-color:Orange;"></div>
+
     <?php
-        foreach($ListofPlanets as $Planet){
+    
+        foreach($ListofShowPlanets->Elements as $Planet){
             /*PlanetConfig*/
-            $PlanetShowSize = ($Planet->kmDiameter/2)/60;
-                $PlanetShowSize *=0.5;
-            
+            foreach($ListofPlanets as $RealPlanets){
+             if($RealPlanets->name == $Planet->name){
+                 $Planet->color = $RealPlanets->hexColor;
+             }  
+            }
             /*Div as planet element*/
             echo"<div ".
                 "id=\"$Planet->name\" ".
-                "class=\"element\" ".
+                "class=\"element\"".
                 "style=\"".
-                    "Left:$PlanetShowSize;".
-                    "Bottom:$PlanetShowSize;".
-                    "Height:".($PlanetShowSize).";".
-                    "Width:".($PlanetShowSize).";".
-                    "background-color:$Planet->hexColor;".
-                    "\">$Planet->planetType</div>\n";
+                    "Left:0;".
+                    "Bottom:0;".
+                    "background-color:$Planet->color;";
+            if(($Planet->radius*2) > 50){
+            echo    "height:".($Planet->radius).";".
+                    "width:".($Planet->radius).";";
+            } else {
+            echo    "height:".($Planet->radius*4).";".
+                    "width:".($Planet->radius*4).";";
+            }
+            
+            echo    "\"></div>\n";
             
             if($Planet->name != "Sol"){
                 /*Script for running the javascript*/
-                $PlanetShowSize*=2.5;
                 echo"<script>\n";
-                echo"PlanetOrbit(\"$Planet->name\", ".($Planet->orbitalPeriodDays).", $Planet->sunDistance, $PlanetShowSize)\n";
+                echo"PlanetOrbit(\"$Planet->name\", $Planet->speed, $Planet->distance,$Planet->radius)\n";
                 echo"</script>\n";
             }
         }
